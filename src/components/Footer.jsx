2f4f8def3bf89f39615a47b798b662ac8ab96c88
DESKTOP-NPTOF76\Gökalp AVCU <avcu219@gmail.com>
2022-05-12 22:13:53 +0300

    add powered by

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,25 +1,13 @@
-import React from 'react'
+import React from "react";
 
 const Footer = () => {
   return (
     <div>
-        <footer className="w3-center w3-black w3-padding-16">
-  <p>
-    Powered by{""}
-    <a
-      href="https://www.w3schools.com/w3css/default.asp"
-      title= "Gökalp Avcu"
-      target="_blank"
-      className="w3-hover-text-green"
-    >
-      w3.css
-      
-    </a>
-  </p>
-</footer>
-
+      <footer className="w3-center w3-black w3-padding-16">
+        <p>© 2022 | Gökalp Avcu 👨‍💻</p>
+      </footer>
     </div>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
